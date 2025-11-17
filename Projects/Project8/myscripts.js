9a8601c2458dc280bbd323990bfc37c6df64c838
BastianHulse <10766082@uvu.edu>
2025-11-17 16:45:40 -0700

    Project8

---
+++ Projects/Project8/myscripts.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+	
+	PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+	if (document.images) {
+		PrintButton1.src = "images/print1.png"
+		PrintButton2.src = "images/print2.png"
+		logo1.src = "images/logo1.jpg"
+		logo2.src = "images/logo2.jpg"
+	}
+
+
+		var imgArray = new Array(
+			'Image1Lg.jpg',
+			'Image2Lg.jpg',
+			'Image3Lg.jpg',
+			'Image4Lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'American Goldfinch Perched on a Pot',
+			'Heron in a Lake Fishing for Dinner',
+			'Two Pelicans Staring into the Others Eyes',
+			'Woodstock Duck Vainly Staring at its Reflection'
+			
+		);
+			
+		var imgPath = "images/FullSize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      		textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
+		
